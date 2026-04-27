db084788d36a21914fe1f4c76a10f2567d4bf44f
Mamta <mamta.bourai2107@gmail.com>
2026-04-27 19:21:49 +0530

    changed apple to orange

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-resource "azurerm_resource_group" "apple" {
+resource "azurerm_resource_group" "orange" {
   for_each = var.mapvar
   name     = each.value.name
   location = each.value.location
 }
+
+
